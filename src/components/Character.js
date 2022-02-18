bc17921211b3ccbca4de49be0994a606e753449e
Devon Curtis <87959309+DevonCurtis@users.noreply.github.com>
2022-02-18 09:41:48 -0500

    added styled component CharDiv and created cards with character info

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,10 +3,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const Character = () => {
+const Character = (props) => {
+    const {info} = props;
+
+
   return (
-    <div>Character</div>
+    <CharDiv>
+        <div>
+            <h3>{info.name}</h3>
+            <p>BirthYear: {info.birth_year}</p>
+            <p>Gender: {info.gender}</p>
+            <p>Mass: {info.mass}</p>
+        </div>
+    </CharDiv>
   )
 }
+
+const CharDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    background-color: #f5d3a1a3;
+    color: #44e3e;
+    text-shadow: 1px 1px 5px #fff;
+    border: 1px solid #d2d2d2;
+    box-shadow: 0px 1px 6px -2px #807f7f;
+    border-radius: 8px;
+    margin-top: 16px;
+    padding: 16px;
+`;
 
 export default Character

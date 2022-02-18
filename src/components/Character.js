b82b489c5051ce531a7cbcc4370144e2c3851a17
Devon Curtis <87959309+DevonCurtis@users.noreply.github.com>
2022-02-18 09:14:42 -0500

    created boilerplate needed for character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Character = () => {
+  return (
+    <div>Character</div>
+  )
+}
+
+export default Character
